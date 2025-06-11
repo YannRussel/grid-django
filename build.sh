381353bf6db14cc0f9188574396284694800884d
Yann Russel <yann.russel.webwriter@gmail.com>
2025-06-11 15:51:49 +0100

    Ajout du superuser auto dans build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,15 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crée un superuser avec numéro de téléphone s'il n'existe pas
+echo "
+from accounts.models import Controleur
+if not Controleur.objects.filter(phone_number='${DJANGO_SU_PHONE}').exists():
+    Controleur.objects.create_superuser(
+        phone_number='${DJANGO_SU_PHONE}',
+        email='${DJANGO_SU_EMAIL}',
+        password='${DJANGO_SU_PASS}',
+        role='admin'
+    )
+" | python manage.py shell
