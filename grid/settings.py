1d4e73c02999ca770495c18a5e3544e433da1910
Yann Russel <yann.russel.webwriter@gmail.com>
2025-06-11 15:39:43 +0100

    Ajout des fichiers de migration

--- grid/settings.py
+++ grid/settings.py
@@ -86,13 +86,18 @@ WSGI_APPLICATION = 'grid.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.getenv("RENDER"):
+    DATABASES = {
+        'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
